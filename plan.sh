76a0c5ecedb0e16a6237b1137d54131b8b339885
Darron Froese <darron@froese.org>
2016-06-14 22:29:39 -0600

    Expose a few ports.

--- plan.sh
+++ plan.sh
@@ -7,6 +7,7 @@ pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}
 pkg_shasum=abdf0e1856292468e2c9971420d73b805e93888e006c76324ae39416edcf0627
 pkg_bin_dirs=(bin)
 pkg_build_deps=(core/cacerts)
+pkg_expose=(8300 8301 8302 8500 8600)
 
 do_build() {
   return 0
