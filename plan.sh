905b43f7558530381854302a64efc26da2b59375
Darron Froese <darron@froese.org>
2016-06-14 22:18:49 -0600

    First commit - working single bootstrapped Consul node.

---
+++ plan.sh
@@ -0,0 +1,18 @@
+pkg_name=consul
+pkg_origin=darron
+pkg_version=0.6.4
+pkg_maintainer="Darron Froese <darron@froese.org>"
+pkg_license=('mpl2')
+pkg_source=https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip
+pkg_shasum=abdf0e1856292468e2c9971420d73b805e93888e006c76324ae39416edcf0627
+pkg_bin_dirs=(bin)
+pkg_build_deps=(core/cacerts)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  mkdir -p $pkg_prefix/bin
+  cp /hab/cache/src/consul $pkg_prefix/bin/
+}
